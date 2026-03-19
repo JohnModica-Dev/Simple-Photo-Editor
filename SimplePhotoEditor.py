d9fe6264ac6820a9dcca5389b7267f49eaf88d22
MrThiccster <modicajt@gmail.com>
2026-03-19 16:42:48 -0500

    Added and updated Ui elements, History now works and the redo button also is functional. Added a save button to be implimented on in the future.

--- SimplePhotoEditor.py
+++ SimplePhotoEditor.py
@@ -18,10 +18,11 @@ rotationAngle = 0
 Image_Activated = False
 eOrder = []
 
-
+# BuildMainWindow is the function that builds the window with all the eding features for the user, like the import button, rotate button and the size button. Need to rearrange them more
+# to look nicer.
 def BuildMainWindow():
     global BASE_DIR, SetSizeWidth, SetSizeHeight
-    ImageNames = ['10.png','-10.png','DownArrow.png','UpArrow.png','ImportPhoto.png','TextEditorTitle.png']
+    ImageNames = ['10.png','-10.png','DownArrow.png','UpArrow.png','ImportPhoto.png','TextEditorTitle.png','Save.png','Undo.png','Redo.png','SetSize.png']
     uiImages = []
     x = 0
     while x != len(ImageNames):
@@ -46,35 +47,43 @@ def BuildMainWindow():
     
     WidthUBtn = tk.Button(root,image=uiImages[3],bg='#c3e8bd',activebackground='#c3e8bd',command=lambda: WidthUpDown(10))
     WidthUBtn.image = uiImages[3]
-    WidthUBtn.place(x=200,y=500)
+    WidthUBtn.place(x=140,y=500)
     
     WidthDBtn = tk.Button(root,image=uiImages[2],bg='#c3e8bd',activebackground='#c3e8bd', command=lambda: WidthUpDown(-10))
     WidthDBtn.image = uiImages[2]
-    WidthDBtn.place(x=300,y=500)
+    WidthDBtn.place(x=365,y=500)
     
     HeightDBtn = tk.Button(root,image=uiImages[2],bg='#c3e8bd',activebackground='#c3e8bd', command=lambda: HeightUPDown(-10))
     HeightDBtn.image = uiImages[2]
-    HeightDBtn.place(x=300,y=700)
+    HeightDBtn.place(x=365,y=719)
     
     heightUBtn = tk.Button(root,image=uiImages[3],bg='#c3e8bd',activebackground='#c3e8bd', command=lambda: HeightUPDown(10))
     heightUBtn.image = uiImages[3]
-    heightUBtn.place(x=200,y=700)
+    heightUBtn.place(x=140,y=719)
     
-    SetSizeBtn = tk.Button(root,width=10,height=10,bg='#c3e8bd',activebackground='#c3e8bd', command=lambda: ResizeImg())
-    SetSizeBtn.place(x=300,y=800)
+    SetSizeBtn = tk.Button(root,bg='#c3e8bd',activebackground='#c3e8bd', command=lambda: ResizeImg(),image=uiImages[9])
+    SetSizeBtn.image = uiImages[9]
+    SetSizeBtn.place(x=50,y=600)
     
-    SetSizeWidth = tk.Text(root, width=10, height=10)
-    SetSizeWidth.place(x = 500, y = 800)
+    SetSizeWidth = tk.Text(root, width=3, height=1,font=["Helvetica", 53],padx=5, bg='#c3e8bd',foreground='grey',)
+    SetSizeWidth.place(x = 230, y = 500)
     
-    SetSizeHeight = tk.Text(root, width=10, height=10)
-    SetSizeHeight.place(x = 500, y= 900)
+    SetSizeHeight = tk.Text(root, width=3, height=1, font=['Helvetica',53], padx=5, bg='#c3e8bd', foreground='grey')
+    SetSizeHeight.place(x = 230, y= 719)
     
-    UnDoBtn = tk.Button(root, width=10, height=10, bg='#c3e8bd', activebackground='#c3e8bd', command=lambda: redo_or_undo('undo'))
+    UnDoBtn = tk.Button(root, bg='#c3e8bd', activebackground='#c3e8bd', command=lambda: redo_or_undo('undo'), image=uiImages[7])
+    UnDoBtn.image = uiImages[7]
     UnDoBtn.place(x=900,y=600)
     
-    ReDoBtn = tk.Button(root, width=10,height=10, bg='#c3e8bd', activebackground='#c3e8bd', comman=lambda: redo_or_undo('redo'))
+    ReDoBtn = tk.Button(root, bg='#c3e8bd', activebackground='#c3e8bd', command=lambda: redo_or_undo('redo'), image=uiImages[8])
+    ReDoBtn.image = uiImages[8]
     ReDoBtn.place(x=900,y=900)
+    
+    SaveBtn = tk.Button(root, bg='#c3e8bd', activebackground='#c3e8bd', image=uiImages[6])
+    SaveBtn.image = uiImages[6]
+    SaveBtn.place(x=50, y=370)
 
+# import_file functioin allows the user to import a file and als allows a user to save an image before importing another file
 def import_file(): #imports an image of the users choosing, to be edited.
     global Iimg, Cimg, Image_Activated
     if Image_Activated:
@@ -89,12 +98,15 @@ def import_file(): #imports an image of the users choosing, to be edited.
     except:
         return
     
+# reset_edits function resets the image so that if the image is deleted from the program, so is all the edit orders, history and rotation angle and width, height
 def reset_edits():
-    global rotationAngle, hIstory, Image_Activated
+    global rotationAngle, hIstory, Image_Activated, eOrder
     Image_Activated = False
     rotationAngle = 0
     hIstory = []
+    eOrder = []
 
+# ask_to_save function asks the user to save their work through a separate window. This makes all the other windows inactive whils this happens
 def ask_to_save():
     global IPWindow, Image_Activated,SWindow
     SWindow = tk.Toplevel(root, height=200, width=400)
@@ -105,6 +117,7 @@ def ask_to_save():
     SWindow.wait_window(window=SWindow)
     Image_Activated = False
     
+# Save_Image function actually saves the image that the user is working on.
 def Save_Image():
     global Cimg, SWindow, IPWindow, Iimg
     print(Cimg.info)
@@ -113,8 +126,7 @@ def Save_Image():
     IPWindow.destroy()
     
     
-
-    
+# Build_Save_Window function builds the save window that asks the user to save their work before importing another image.
 def Build_Save_Window():
     global SWindow, IPWindow, Image_Activated
     SWindow.maxsize(400,200)
@@ -124,7 +136,7 @@ def Build_Save_Window():
     NBTN = tk.Button(master=SWindow,width=4,height=1, text='NO',command=lambda:(SWindow.destroy(), IPWindow.destroy(), reset_edits(), import_file()))
     NBTN.place(x=250,y=100)
     
-
+# BuildPreviewWwindow function builds the preview window where the user can actually see their images and information
 def BuildPreviewWindow(): #initial making of the image preview window
     global IPWindow, IPFrame, root, pReview, Image_Activated
     Image_Activated = True
@@ -136,6 +148,8 @@ def BuildPreviewWindow(): #initial making of the image preview window
     IPWindow.mainloop()
     
 
+# UpdatePreviewWindow funtion allows for the image to be shown to the user on a separate window, it makes sure to resize it so that the image can be seen by the user and the window doesnt cut
+# it off. I plan to add statistics to the bottom like the Size of the image, the format and the name of the image.
 def UpdatePreviewWindow(): #updates the preview window when maing changes #imcomplete
     global IPWindow, IPFrame, Cimg, Pimg, hIstory, eOrder
     if IPFrame != 0:
@@ -157,6 +171,7 @@ def UpdatePreviewWindow(): #updates the preview window when maing changes #imcom
     ImagePreview.place(x=IPFrame['width']//2,y=IPFrame['height']//2,anchor='center')
     
     
+# The ResizeImg function is used to resize the image to a certain width and certain height that the user wants to put it as.
 def ResizeImg(): #resizes the given image 
     global Cimg, SetSizeWidth, SetSizeHeight, eOrder
     RotateImg(0)
@@ -181,6 +196,9 @@ def ResizeImg(): #resizes the given image
     reset_undo()
     UpdatePreviewWindow()
     
+# HeightUPDown and WidthUPDown function makes sure to add the height and width to the image editing order, it does this through adding up similar height and width adujstments, for example if
+# the user wanted to add 10 to width, then add another 70 right after, its a addition of 80 to the width instead of the 10 and then the 70, this allows for further image disruption
+# to be avoided.
 def WidthUpDown(Value):
     global Cimg, eOrder, rotationAngle
     try:
@@ -197,6 +215,9 @@ def WidthUpDown(Value):
     reset_undo()
     UpdatePreviewWindow()
     
+# HeightUPDown and WidthUPDown function makes sure to add the height and width to the image editing order, it does this through adding up similar height and width adujstments, for example if
+# the user wanted to add 10 to width, then add another 70 right after, its a addition of 80 to the width instead of the 10 and then the 70, this allows for further image disruption
+# to be avoided.
 def HeightUPDown(Value):
     global Cimg, eOrder, rotationAngle
     try:
@@ -213,6 +234,8 @@ def HeightUPDown(Value):
     reset_undo()
     UpdatePreviewWindow()
 
+# RotateImg function makes sure to add the degrees of rotation to the image that the user requests. instead of rotating the image in certain degrees and then stopping, it makes 
+# sure to add the degrees together to further prevent image clarity disruption. For example, instead of a 10 degreee turn, then another 10 degree turn, its now a 20 degree turn
 def RotateImg(degrees): #rotates the given image
     global Cimg,Iimg, rotationAngle,eOrder, undo_Check
     if (rotationAngle == 360) or (rotationAngle == -360):
@@ -230,31 +253,47 @@ def RotateImg(degrees): #rotates the given image
     reset_undo()
     UpdatePreviewWindow()
     
+
+# reset_undo function makes sure to clear the history (hIstory) and sets the undo flag (undo_Check) to false
 def reset_undo():
     global hIstory, undo_Check
     hIstory = []
     undo_Check = False
 
+
+# redo_o_undo function works by taking the editing order (eOrder), History (hIstory) and the undo check flag (undo_Check). It checks that that when the undo button is pressed,
+# atleast one edit is in the editing order before proceeding, if there is non in the editing order, then it cancels further actions. The items in the editing order does not matter
+# to the undo button so the undo button by passes the return and goes onto the function. The undo button makes it so that it adds it contents of the last edit to the history list
+# and then deletes the contents of the last edit to the editing order, it also makes sure to check the undo flag to True so that the redo button works as intended. The redo button
+# makes sure to add the last added edit from undo to the editing order and then deletes that form the History, this allows for the editing order to add back what the user tried to 
+# delete, If the undo flag is not True, then the funtion exits.
 def redo_or_undo(Text):
     global eOrder, hIstory, undo_Check
     if len(eOrder) < 1:
-        return
+        if Text == 'undo':
+            return
     if Text == 'undo':
         hIstory.append(eOrder[len(eOrder)-2])
         hIstory.append(eOrder[len(eOrder)-1])
         eOrder.pop(len(eOrder)-1)
         eOrder.pop(len(eOrder)-1)
         undo_Check = True
+        print(hIstory)
         UpdatePreviewWindow()
     elif Text == 'redo':
-        if undo_Check == True:
-            eOrder.append(hIstory[len(hIstory)-2])
-            eOrder.append(hIstory[len(hIstory)-1])
-            hIstory.pop(len(hIstory)-1)
-            hIstory.pop(len(hIstory)-1)
-            UpdatePreviewWindow()
+        if len(hIstory) > 0:
+            if undo_Check == True:
+                eOrder.append(hIstory[len(hIstory)-2])
+                eOrder.append(hIstory[len(hIstory)-1])
+                hIstory.pop(len(hIstory)-1)
+                hIstory.pop(len(hIstory)-1)
+                UpdatePreviewWindow()
+            else:
+                return
             
 
+# BuildImage function allows for the image to be built based on the editing order (eOrder) of the image to be made. It cycles through the entire editing order for this to happen
+# and makes changes as it iterates through it.
 def BuildImage():
     global eOrder, Cimg, Iimg
     x = 0 
